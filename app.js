d0c27d11a099327aaa6b644a468f7ed1f8dae333
Joseph-Franquis <joseph.franquis.7@gamil.com>
2022-06-28 15:14:38 +0200

    start

--- app.js
+++ app.js
@@ -13,29 +13,28 @@ const mysql = require('mysql');
 
 
 
-const connection = mysql.createConnection({
-  host:'db4free.net',
-  user: 'joseph_23',
-  password:'29866830J@s',
-  database: 'arbol'
-});
+// const connection = mysql.createConnection({
+//   host:'db4free.net',
+//   user: 'joseph_23',
+//   password:'29866830J@s',
+//   database: 'mistramites'
+// });
 
-connection.connect(function(err) {
-  console.log(1);
-  if (err){
-    console.log("Esto ha fallado :"+err);
-  } else{
-    console.log("Connected!");
-    connection.query("SELECT * FROM usuarios", function (err, result, fields) {
-      if (err){ console.log("Esto ha fallado :"+err) }else{
-        result.forEach(element => {
-          console.log(element.id);
-        });
-      }
-    });
-  }
+// connection.connect(function(err) {
+//   if (err){
+//     console.log("Esto ha fallado :"+err);
+//   } else{
+//     console.log("Connected!");
+//     connection.query("SELECT * FROM usuarios", function (err, result, fields) {
+//       if (err){ console.log("Esto ha fallado :"+err) }else{
+//         result.forEach(element => {
+//           console.log("id de usuarios =>"+element.id);
+//         });
+//       }
+//     });
+//   }
 
-});
+// });
 
 //para permitir la conexion con el servidor de frontend
 app.use(cors());
@@ -57,7 +56,7 @@ app.get('/', function (req, res) {
 });
 
 
-
+//coneccion del sokect
 io.on('connection', function (socket) {
 
   const handshake = socket.id;
@@ -65,19 +64,19 @@ io.on('connection', function (socket) {
   let { id } = socket.handshake.query;
   let { id_recep } = socket.handshake.query;
 
-  console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${nameRoom}`);
-  socket.join(nameRoom)
-
-  // console.log(`Hola dispositivo ${handshake} `);
+  console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${id}`);
 
   socket.on('chat-gen', (msg)=>{
-      io.emit('chat-gen', msg);
+    // connection.query("INSERT INTO mensajes values ()", function (err, result, fields) {
+
+    // });
+    io.emit('chat-gen', msg);
   });
 
-  socket.on('chat'+id+id_recep, (msg)=>{
+  socket.on('chat1', (msg)=>{
     console.log(msg)
     io.emit('chat1', msg);
-});
+  });
 });
 
 server.listen(5000, function () {
